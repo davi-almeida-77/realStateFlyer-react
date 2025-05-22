11f182dd4b25b4dce50a8927be8175be9b35fb8b
davi-almeida-77 <DaviAlmeidaDEV@Hotmail.com>
2025-05-22 09:06:11 -0300

    [ADD] React + Vite + TailwindCSS

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-500 text-white text-4xl font-bold">
+      Tailwind funcionando 100%! 🚀
+    </div>
+  );
+}
+
+
+export default App;
