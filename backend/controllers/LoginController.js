745992ee1d160a2dfec86b102cb3b4af604696aa
davi-almeida-77 <DaviAlmeidaDEV@Hotmail.com>
2025-05-22 18:19:09 -0300

    [ADD] Controllers

---
+++ backend/controllers/LoginController.js
@@ -0,0 +1,12 @@
+exports.login_user = (req, res) => {
+  const { email, password } = req.body;
+
+  if ( !email || !password ) {
+    return res.status(400).json({ message: 'email and password invalid' });
+  }
+
+  if ( email && password ) {
+    return res.status(200).json({ message: 'Sucessful Login' });
+  }
+  
+};
